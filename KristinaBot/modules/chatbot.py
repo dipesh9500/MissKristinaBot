2540826a81fabbb8f29ea786a2d35d30e6df95ba
Dipesh <83916289+xdipesh@users.noreply.github.com>
2021-06-19 16:07:05 +0530

    Update chatbot.py

--- KristinaBot/modules/chatbot.py
+++ KristinaBot/modules/chatbot.py
@@ -167,3 +167,10 @@ __handlers__ = [
     CHATBOT_HANDLER,
     LIST_CB_CHATS_HANDLER,
 ]
+help = """
+ Here is the help for the chatbot module:
+
+<b> Chatbot </b>
+Kristina AI 3.0 IS THE ONLY AI SYSTEM WHICH CAN DETECT & REPLY UPTO 200 LANGUAGES
+ - /chatbot ON/OFF: Enables and disables AI Chat mode (EXCLUSIVE)
+ - /chatbot EN : Enables English only chatbot
