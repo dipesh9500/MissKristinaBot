f78e2ef301d60d3b286eccf924a1a2ad6a65b281
Dipesh <83916289+xdipesh@users.noreply.github.com>
2021-06-20 16:55:12 +0530

    Update __telegraph.py

--- KristinaBot/modules/__telegraph.py
+++ KristinaBot/modules/__telegraph.py
@@ -12,7 +12,7 @@ r = telegraph.create_account(short_name=dipesh)
 auth_url = r["auth_url"]
 
 
-@register(pattern="^/t(xm|xt) ?(.*)")
+@register(pattern="^/t(gm|gt) ?(.*)")
 async def _(event):
     if event.fwd_from:
         return
